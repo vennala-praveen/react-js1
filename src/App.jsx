8394c2179d672c3e31798b665141fbaa80357d86
Praveen Vennala <praveen98vennala@gmail.com>
2025-06-22 19:27:20 +0530

    react advice apis

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,24 @@ import axios from 'axios'
 const App = () => {
 
   const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
 
   async function getAdvice(){
-    console.log('Hello')
+
+    if(count < 10){
+      console.log('Hello')
     const response = await axios.get('https://api.adviceslip.com/advice');
-    setAdvice(response.data.slip.advice)
+    setAdvice(response.data.slip.advice);
+    setCount(c=>c+1)
+    }
+    else alert('Advice limit reached try again today')
   }
 
   return (
     <div>
       <button onClick={getAdvice}>Get Advice</button>
       <h1>{advice}</h1>
+      <p>I have read {count} pieces of advice today</p>
     </div>
   )
 }
