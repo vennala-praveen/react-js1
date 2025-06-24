ecde5458c9d33de4da276a47b8c7552bc51bc1a7
Praveen Vennala <praveen98vennala@gmail.com>
2025-06-24 12:30:18 +0530

    react advice apis

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ const App = () => {
 
   async function getAdvice(){
 
-    if(count < 10){
-      console.log('Hello')
+    if(count < 20){
     const response = await axios.get('https://api.adviceslip.com/advice');
     setAdvice(response.data.slip.advice);
     setCount(c=>c+1)
@@ -18,10 +17,10 @@ const App = () => {
   }
 
   return (
-    <div>
-      <button onClick={getAdvice}>Get Advice</button>
-      <h1>{advice}</h1>
-      <p>I have read {count} pieces of advice today</p>
+    <div className='container'>
+      <button className='btn' onClick={getAdvice}>Get Advice</button>
+      {advice && <p className='advice'>{advice}</p>}
+      <p className='count'>I have read {count} pieces of advice today</p>
     </div>
   )
 }
